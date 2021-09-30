a3165186607cf9f2d0dc49ae8cadbf07b526ce66
lim chee keat <cheekeat_lim1991@hotmail.com>
2021-09-30 09:40:26 +0800

    Updates

---
+++ precache-manifest.2ef437ba88087a1db09479ebd2badb06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f20279ebb5b7eaf8cf7",
+    "url": "/WAGMIGOTCHI/static/css/main.9004dc0f.chunk.css"
+  },
+  {
+    "revision": "3f20279ebb5b7eaf8cf7",
+    "url": "/WAGMIGOTCHI/static/js/main.3f20279e.chunk.js"
+  },
+  {
+    "revision": "2b2d9c818da7b3cc0bd1",
+    "url": "/WAGMIGOTCHI/static/css/1.c8806d17.chunk.css"
+  },
+  {
+    "revision": "2b2d9c818da7b3cc0bd1",
+    "url": "/WAGMIGOTCHI/static/js/1.2b2d9c81.chunk.js"
+  },
+  {
+    "revision": "7cdfe2b7438f84d8472b",
+    "url": "/WAGMIGOTCHI/static/js/runtime~main.7cdfe2b7.js"
+  },
+  {
+    "revision": "3a96aea7571a2639d9e984f49c64d45f",
+    "url": "/WAGMIGOTCHI/static/media/character.3a96aea7.png"
+  },
+  {
+    "revision": "166ff2d5c5e9103192ed4385a9a38f9c",
+    "url": "/WAGMIGOTCHI/static/media/Love.166ff2d5.ico"
+  },
+  {
+    "revision": "5aaa03c499caea6f6af327c8a94c0696",
+    "url": "/WAGMIGOTCHI/index.html"
+  }
+];
